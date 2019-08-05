ea64218b13664d0fe275ff39552449adbb8a1bad
nitinkundu <50735799+nitinkundu@users.noreply.github.com>
2019-08-05 17:58:18 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
  
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+//   { path: 'login', component: LoginComponent },
   // { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent }
 
 ];
 
