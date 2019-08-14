a5d31c3038ec8f191ca7f61077c06818ce35c7ad
Msarangi <manisa9.ms@gmail.com>
2019-08-14 18:39:42 +0530

    12th commit

--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -11,18 +11,18 @@ export class ServicesService {
   private Url: string;
  
   constructor(private http: HttpClient) {
-    this.Url = 'http://ec2-52-66-245-186.ap-south-1.compute.amazonaws.com:8080/Backend_XFS/api/teamlist';
+    this.Url = 'http://ec2-52-66-245-186.ap-south-1.compute.amazonaws.com:8080/XFSAPP/api/';
   }
  
   public findAll(): Observable<Dashboard> {
-    return this.http.get<Dashboard>(this.Url);
+    return this.http.get<Dashboard>(this.Url );
   }
   
   public save(team: Team) {
-    return this.http.post<Team>(this.Url, +"teamlist");
+    return this.http.post<Team>(this.Url +"addteam", team);
   }
 
   public fetchAll(): Observable<Team> {
-    return this.http.get<Team>(this.Url);
+    return this.http.get<Team>(this.Url +"teamlist");
   }
 }
