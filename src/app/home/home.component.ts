9898e6cee1c6b1448ea8d246b4ea127bcf610331
Msarangi <manisa9.ms@gmail.com>
2019-08-18 09:11:25 +0530

    comitt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { Dashboard } from "../dashboard";
-import { ActivatedRoute,Router, NavigationExtras } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ServicesService } from '../services.service';
+import { Dashboard } from '../dashboard';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
   dash: Dashboard;
-  
-  constructor(private router:Router, private route:ActivatedRoute) { }
+ 
+  constructor(private router: Router, private dashboardService: ServicesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(history.state.data);
-    this.dash = history.state.data;
+    this.dashboardService.findAll().subscribe(data => {
+      console.log(data)
+      this.dash = data;
+    });
   }
   onClick() {
     this.router.navigate(['list']);
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -15,26 +15,12 @@ export class ServicesService {
   public findAll(): Observable<Dashboard> {
     return this.http.get<Dashboard>(this.Url + "teamlist");
   }
-<<<<<<< HEAD
- 
-  public findAll(): Observable<Dashboard> {
-    return this.http.get<Dashboard>(this.Url +"teamlist" );
-  }
-  
-=======
-
->>>>>>> 9e7a395448361decd656780f095361564c272de8
   public save(team: Team) {
     return this.http.post<Team>(this.Url + "addteam", team);
   }
 
-<<<<<<< HEAD
-  public fetchAll() { 
-    return this.http.get(this.Url +"teamlist");
-=======
   public fetchAll() {
     return this.http.get(this.Url + "teamlist");
     // return this.teams;
->>>>>>> 9e7a395448361decd656780f095361564c272de8
   }
 }
